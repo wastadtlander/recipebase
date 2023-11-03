0d087dc12d777f52fed2c15e2eef4a7917c165fa
inseonglee <inseonglee@vt.edu>
2023-11-03 17:44:36 -0400

    add insert, new sql

--- connection.py
+++ connection.py
@@ -26,6 +26,66 @@ def close_database_connection(conn):
         conn.close()
         print("Database connection closed")
 
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    global connection
+    message = ""
+
+    if request.content_length > app.config['MAX_CONTENT_LENGTH']:
+        abort(413)  # Payload Too Large
+
+    if request.method == 'POST':
+        # Get data from form
+        name = request.form['name']
+        email = request.form.get('email', None)  # email can be NULL based on your schema
+        profile_picture = request.files['profile_picture'].read() if 'profile_picture' in request.files else None
+        user_type = request.form['user_type']
+
+        # Generate a UUID for the user_id
+        user_id = str(uuid.uuid4())
+
+        # Insert into database
+        if connection:
+            cursor = connection.cursor()
+            insert_query = (
+                "INSERT INTO user (Name, Email, ProfilePicture, UserType, UserID) VALUES (%s, %s, %s, %s, %s)"
+            )
+            data = (name, email, profile_picture, user_type, user_id)
+
+            try:
+                cursor.execute(insert_query, data)
+                connection.commit()
+                message = "User added successfully!"
+            except mysql.connector.Error as err:
+                print("Error: {}".format(err))
+                message = "Failed to add user."
+            finally:
+                cursor.close()
+        else:
+            message = "No database connection."
+
+    connection_status = "Connected" if connection else "Not Connected"
+    return render_template('add_user.html', message=message, connection_status=connection_status)
+
+
+
+@app.route('/get_image/<int:user_id>')
+def get_image(user_id):
+    cursor = connection.cursor()
+    cursor.execute("SELECT ProfilePicture FROM user WHERE UserID = %s", (user_id,))
+    data = cursor.fetchone()
+    cursor.close()
+    if data and data[0]:
+        return app.response_class(data[0], content_type='image/jpeg') # you might need to adjust the content type
+    else:
+        return "No image", 404
+
+
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global connection
