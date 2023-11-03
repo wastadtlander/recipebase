7cb94df2a876df585169946587aa12520e0e2135
inseonglee <inseonglee@vt.edu>
2023-11-03 19:54:08 -0400

    error fix

--- connection.py
+++ connection.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+import uuid
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024  # sets max upload size to 1MB
 
 config = {
     'host': 'localhost',
@@ -21,10 +23,135 @@ def create_database_connection():
         print("Database connection failed: {}".format(err))
         return None
 
+
 def close_database_connection(conn):
     if conn:
         conn.close()
         print("Database connection closed")
+
+
+def check_connection_status():
+    return "Connected" if connection else "Not Connected"
+
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     global connection
+#     message = ""
+#
+#     if request.method == 'POST':
+#         # Check if open_connection button was clicked
+#         if 'open_connection' in request.form:
+#             connection = create_database_connection()
+#             if connection:
+#                 message = "Connected to the database successfully!"
+#             else:
+#                 message = "Failed to connect to the database."
+#
+#         # Check if close_connection button was clicked
+#         elif 'close_connection' in request.form:
+#             if connection:
+#                 close_database_connection(connection)
+#                 connection = None
+#                 message = "Connection closed successfully!"
+#             else:
+#                 message = "No active connection to close."
+#
+#         # Delete user functionality
+#         elif 'delete_user' in request.form:
+#             username = request.form['username']
+#             if connection:
+#                 cursor = connection.cursor()
+#
+#                 sql = "DELETE FROM Comments WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
+#                 cursor.execute(sql, (username,))
+#
+#                 sql = "DELETE FROM Rating WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
+#                 cursor.execute(sql, (username,))
+#
+#                 sql = "SELECT RecipeID FROM Recipe WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
+#                 cursor.execute(sql, (username,))
+#                 recipe_ids = cursor.fetchall()
+#
+#                 for recipe_id in recipe_ids:
+#                     sql = "DELETE FROM RecipeImage WHERE RecipeID = %s"
+#                     cursor.execute(sql, (recipe_id[0],))
+#
+#                     sql = "DELETE FROM Recipe WHERE RecipeID = %s"
+#                     cursor.execute(sql, (recipe_id[0],))
+#
+#                 sql = "DELETE FROM User WHERE Name = %s"
+#                 cursor.execute(sql, (username,))
+#
+#                 connection.commit()
+#                 cursor.close()
+#                 message = f"User {username} and related data deleted successfully!"
+#             else:
+#                 message = "No active connection. Can't delete user."
+#
+#     connection_status = "Connected" if connection else "Not Connected"
+#     return render_template('index.html', connection_status=connection_status, message=message)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global connection
+
+    # Catch message passed from add_user route
+    message = request.args.get('message', "")
+
+    if request.method == 'POST':
+        # Check if open_connection button was clicked
+        if 'open_connection' in request.form:
+            connection = create_database_connection()
+            if connection:
+                message = "Connected to the database successfully!"
+            else:
+                message = "Failed to connect to the database."
+
+        # Check if close_connection button was clicked
+        elif 'close_connection' in request.form:
+            if connection:
+                close_database_connection(connection)
+                connection = None
+                message = "Connection closed successfully!"
+            else:
+                message = "No active connection to close."
+
+        # Delete user functionality
+        elif 'delete_user' in request.form:
+            username = request.form['username']
+            if connection:
+                cursor = connection.cursor()
+
+                sql = "DELETE FROM Comments WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
+                cursor.execute(sql, (username,))
+
+                sql = "DELETE FROM Rating WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
+                cursor.execute(sql, (username,))
+
+                sql = "SELECT RecipeID FROM Recipe WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
+                cursor.execute(sql, (username,))
+                recipe_ids = cursor.fetchall()
+
+                for recipe_id in recipe_ids:
+                    sql = "DELETE FROM RecipeImage WHERE RecipeID = %s"
+                    cursor.execute(sql, (recipe_id[0],))
+
+                    sql = "DELETE FROM Recipe WHERE RecipeID = %s"
+                    cursor.execute(sql, (recipe_id[0],))
+
+                sql = "DELETE FROM User WHERE Name = %s"
+                cursor.execute(sql, (username,))
+
+                connection.commit()
+                cursor.close()
+                message = f"User {username} and related data deleted successfully!"
+            else:
+                message = "No active connection. Can't delete user."
+
+    connection_status = "Connected" if connection else "Not Connected"
+    return render_template('index.html', connection_status=connection_status, message=message)
 
 
 
@@ -67,7 +194,8 @@ def add_user():
             message = "No database connection."
 
     connection_status = "Connected" if connection else "Not Connected"
-    return render_template('add_user.html', message=message, connection_status=connection_status)
+    # return render_template('add_user.html', message=message, connection_status=connection_status)
+    return redirect(url_for('index', message=message))
 
 
 
@@ -83,47 +211,6 @@ def get_image(user_id):
         return "No image", 404
 
 
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    global connection
-    if request.method == 'POST':
-        if 'open_connection' in request.form:
-            connection = create_database_connection()
-        elif 'close_connection' in request.form:
-            close_database_connection(connection)
-            connection = None 
-        elif 'delete_user' in request.form:
-            username = request.form['username']
-            if connection:
-                cursor = connection.cursor()
-
-                sql = "DELETE FROM Comments WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
-                cursor.execute(sql, (username,))
-
-                sql = "DELETE FROM Rating WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
-                cursor.execute(sql, (username,))
-
-                sql = "SELECT RecipeID FROM Recipe WHERE UserID = (SELECT UserID FROM User WHERE Name = %s)"
-                cursor.execute(sql, (username,))
-                recipe_ids = cursor.fetchall()
-
-                for recipe_id in recipe_ids:
-                    sql = "DELETE FROM RecipeImage WHERE RecipeID = %s"
-                    cursor.execute(sql, (recipe_id[0],))
-
-                    sql = "DELETE FROM Recipe WHERE RecipeID = %s"
-                    cursor.execute(sql, (recipe_id[0],))
-
-                sql = "DELETE FROM User WHERE Name = %s"
-                cursor.execute(sql, (username,))
-
-                connection.commit()
-                cursor.close()
-    connection_status = "Connected" if connection else "Not Connected"
-    return render_template('index.html', connection_status=connection_status)
 
 if __name__ == '__main__':
     app.run(debug=True)
